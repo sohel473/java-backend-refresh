82d5d732cceaa0acb26c4e3015bc932aa9c45bfe
sohel473 <absohel47@gmail.com>
2025-12-30 00:39:41 +0100

    examples of primitive and reference types

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,22 @@ public class Main {
         // print current date and time
         System.out.println("Current date and time: " + date);
 
+        // primitve vs reference
+        int a = 10; // primitive type
+        int b = a; // copy value of a to b
+        b = 20; // change value of b
+        System.out.println("Primitive types:");
+        System.out.println("a: " + a); // a remains 10
+        System.out.println("b: " + b); // b is now 20
+
+        Date date1 = new Date(); // reference type
+        Date date2 = date1; // copy reference of date1 to date2
+        date2.setTime(0); // change date2 to epoch time
+        System.out.println("Reference types:");
+        System.out.println("date1: " + date1); // date1 is also changed
+        System.out.println("date2: " + date2); // date2 is epoch time
+
+
+
     }
 }
