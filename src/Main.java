0847bd3a890f33103a8cfda574a507be160f4337
sohel473 <absohel47@gmail.com>
2025-12-30 01:08:35 +0100

    examples of string immutability

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,39 @@ public class Main {
         // print current date and time
         System.out.println("Current date and time: " + date);
 
-        // primitve vs reference
+        // primitive vs reference
         int a = 10; // primitive type
         int b = a; // copy value of a to b
+        System.out.println("Before changing b:");
+        System.out.println("a: " + a); // a is 10
+        System.out.println("b: " + b); // b is 10
         b = 20; // change value of b
-        System.out.println("Primitive types:");
+        System.out.println("After changing b:");
         System.out.println("a: " + a); // a remains 10
         System.out.println("b: " + b); // b is now 20
 
-        Date date1 = new Date(); // reference type
+        // Reference type example
+        Date date1 = new Date(); // create a new Date object
         Date date2 = date1; // copy reference of date1 to date2
+        System.out.println("Before changing date2:");
+        System.out.println("date1: " + date1); // print date1
+        System.out.println("date2: " + date2); // print date2
         date2.setTime(0); // change date2 to epoch time
-        System.out.println("Reference types:");
+        System.out.println("After changing date2:");
         System.out.println("date1: " + date1); // date1 is also changed
-        System.out.println("date2: " + date2); // date2 is epoch time
+        System.out.println("date2: " + date2); // date2 is changed
+
+        // String immutability
+        String str1 = "Hello";
+        String str2 = str1; // copy reference of str1 to str2
+        System.out.println("before changing str2:");
+        System.out.println("str1: " + str1); // print str1
+        System.out.println("str2: " + str2); // print str2
+        str2 = "World"; // change str2 to a new string
+        System.out.println("after changing str2:");
+        System.out.println("str1: " + str1); // str1 remains "Hello"
+        System.out.println("str2: " + str2); // str2 is now "World"
+
 
 
 
